d2ba5caba003c6317a5eec9e0bc779f705e7872a
GTM-Winter <dimastriana03@gmail.com>
2022-06-22 10:22:55 +0700

    fix bug

--- script.js
+++ script.js
@@ -15,9 +15,12 @@ container.addEventListener("click", function(e) {
       my_img.classList.remove('fade');
     }, 500);
     thumbs.forEach(function(thumb) {
+      // jika ada class active
       if(thumb.classList.contains('active')){
-        thumb.classList.remove('active')
+        // remove class nya
+        thumb.classList.remove('active');
       }else {
+        // jika tidak ada tambahkan class active di img yg di click
         e.target.classList.add('active');
       }
     });
